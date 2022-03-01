3182ae49522e771c216aaf425414afe4843f6a1d
Zhi Ying Torri Ho <zhiyingho99@gmail.com>
2022-03-02 02:16:36 +0800

    Initial commit

---
+++ src/components/home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'hi'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
